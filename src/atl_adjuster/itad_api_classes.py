a47679eeecf9d436b4059075d79024ea18f52d69
Daniel Cormier <git@drc97.com>
2022-06-26 10:25:20 -0400

    fixed lines too long and None error

--- src/atl_adjuster/itad_api_classes.py
+++ src/atl_adjuster/itad_api_classes.py
@@ -1,5 +1,6 @@
 """
-This module contains the underlying classes used to interact with the ITAD API, including:
+This module contains the underlying classes used to interact with the ITAD API,
+including:
     - Pydantic models for the JSON data returned by the API
     - Interfaces for the ITAD API
 """
@@ -80,10 +81,14 @@ class APIGetLowest(ABC):
     Interface defining a method for getting the lowest price of a game
     """
 
-    def get_lowest_price_for_game(self, plain_name: str, lowest: Lowest) -> float:
+    def get_lowest_price_for_game(self,
+                                  plain_name: str,
+                                  lowest: Lowest) -> float:
         return lowest.data[plain_name].price
 
-    def get_lowest_price_for_games(self, plain_names: Iterable[str], lowest: Lowest) -> Iterable[float]:
+    def get_lowest_price_for_games(self,
+                                   plain_names: Iterable[str],
+                                   lowest: Lowest) -> Iterable[float]:
         return (lowest.data[plain_name].price for plain_name in plain_names)
 
 
@@ -91,7 +96,9 @@ class APIGetPlain(ABC):
     """
     Interface defining a method for getting the plain name of a game
     """
-    id_pattern = re.compile(r'^https://store\.steampowered\.com/(\w+?/\d+?)/\w+/?$')
+    id_pattern = re.compile(
+        r'^https://store\.steampowered\.com/(\w+?/\d+?)/\w+/?$'
+    )
 
     @abstractmethod
     def get_plain_name_for_game_by_name(self, game_name: str) -> str:
@@ -99,9 +106,12 @@ class APIGetPlain(ABC):
 
     def get_plain_name_for_game_by_steam_url(self, game_url: str) -> str:
         """
-        Converts a Steam URL to a Steam ID and then calls get_plain_name_for_game_by_steam_id
+        Converts a Steam URL to a Steam ID and then calls
+        get_plain_name_for_game_by_steam_id
         """
-        return self.get_plain_name_for_game_by_steam_id(self.steam_url_to_steam_id(game_url))
+        return self.get_plain_name_for_game_by_steam_id(
+            self.steam_url_to_steam_id(game_url)
+        )
 
     @abstractmethod
     def get_plain_name_for_game_by_steam_id(self, game_id: str) -> str:
@@ -111,4 +121,13 @@ class APIGetPlain(ABC):
         """
         Converts a Steam URL to a Steam ID
         """
-        return self.id_pattern.match(steam_url).group(1)
+        match self.id_pattern.match(steam_url):
+            case match if isinstance(match, re.Match):
+                return match.group(1)
+            case None:
+                raise ValueError(
+                    f'Could not convert Steam URL "{steam_url}" to Steam ID'
+                )
+        raise ValueError(
+            f'Could not convert Steam URL "{steam_url}" to Steam ID'
+        )
